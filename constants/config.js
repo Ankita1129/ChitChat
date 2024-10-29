631c721c0d217ad01a772752194047a52222ac8b
Ankita Singh <88998746+Ankita1129@users.noreply.github.com>
2024-10-29 11:26:48 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,8 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
+    // "http://localhost:5173",
+    // "http://localhost:4173",
+    "https://chit-chat-frontend-phi.vercel.app/",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
